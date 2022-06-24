524450e43b427ca609fae243d4ef451e396de3fc
cyrax <bskaxe@gmail.com>
2022-06-24 15:34:16 +0700

    params

--- bin/golden_presenter.dart
+++ bin/golden_presenter.dart
@@ -2,24 +2,31 @@ import 'dart:io';
 
 import 'package:golden_presenter/golden_image_factory.dart';
 import 'package:golden_presenter/golden_presenter_bloc.dart';
+import 'package:golden_presenter/parser.dart';
 import 'package:golden_presenter/structure_generator.dart';
 import 'package:golden_presenter/structure_presenter.dart';
 import 'package:golden_presenter/structure_saver.dart';
 
-void main(List<String> arguments) async {
+void main(List<String> args) async {
+  final parser = Parser(args: args);
+
+  if (parser.isHelp) {
+    print(parser);
+  }
+
   final StructureGenerator structureGenerator = FileStructureGenerator(
-    directoryProvider:
-        Directory('/Users/cyrax/projects/work/international-mobile-app/test'),
+    directoryProvider: Directory(parser.path),
     goldenImageFactory: GoldenImageFactory(
-      devicePattern: r'^(.*?)[\%\(\.]',
-      localePattern: r'\((.*?)\)',
-      themePattern: r'%5B(.*?)%5D',
+      devicePattern: parser.devicePattern,
+      localePattern: parser.localePattern,
+      themePattern: parser.themePattern,
     ),
-    pathToTest: 'screens',
-    goldenPathName: 'golden',
+    pathToTest: parser.testPath,
+    goldenPathName: parser.goldenFolder,
   );
   final StructurePresenter structurePresenter = HtmlStructurePresenter();
-  final StructureSaver structureSaver = FileStructureSaver();
+  final StructureSaver structureSaver =
+      FileStructureSaver(outputFileName: parser.output);
 
   final goldenPresenterBloc = GoldenPresenterBloc(
     structureGenerator: structureGenerator,
